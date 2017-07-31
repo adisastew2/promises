416a58f526d1dde9aad17316da2d394049a2c995
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-07-30 20:11:39 -0700

    Pass tests for callBack review

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,32 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, cb) {
+
+  fs.readFile(filePath, 'utf8', (err, content) =>{ 
+    if (err) {
+      // console.log('fs.readFile failed :(\n', err);
+      cb(err, null);
+    } else {
+      // console.log('fs.readFile successfully completed :)\n', content);
+      var firstLine = content.split('\n');
+      cb(err, firstLine[0]);
+    }
+  });
+
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, cb) {
+  
+  request(url, function (err, res, body) {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, res.statusCode);
+    }
+  
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
